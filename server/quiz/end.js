d87e64e69dbaaa8bd6d7ab10a6921210142b3735
Tomislav Dinev <dinevtomislav@gmail.com>
2023-04-20 13:09:42 +0100

    slight adjustments on end js file

--- server/quiz/end.js
+++ server/quiz/end.js
@@ -19,6 +19,6 @@ saveHighScore=e=>{
         highScores.sort ((a, b) => b.score - a.score);
         highScores.splice(5);
         localStorage.setItem("highScores", JSON.stringify(highScores));
-        //window.location.assign("index.html");
+        window.location.assign("index.html");
         console.log(highScores)
 }
