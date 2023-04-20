cf4b26794931cab87f8ea380540b1fa9dbd046d9
Tomislav Dinev <dinevtomislav@gmail.com>
2023-04-20 12:54:52 +0100

    MVP has been achieved

--- server/quiz/game.js
+++ server/quiz/game.js
@@ -9,20 +9,351 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [];
 
-fetch('questions.json')
-    .then((res) => {
-        return res.json();
-    })
-    .then((loadedQuestions) => {
-        questions = loadedQuestions;
-        startGame();
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+let questions = [
+        {
+            question: "In The Lord of the Rings trilogy, what is the name of the wizard who helps Frodo and the other characters on their journey?",
+            choice1: "Gandalf",
+            choice2: "Dumbledore",
+            choice3: "Merlin",
+            choice4:  "Yoda",
+            answer: 1
+            },
     
+    {
+        question: "What is the name of the longest word in the English language?",
+        choice1: "Pneumonoultramicroscopicsilicovolcanoconiosis",
+        choice2: "Supercalifragilisticexpialidocious",
+        choice3: "Antidisestablishmentarianism",
+        choice4:  "Hippopotomonstrosesquippedaliophobia",
+        answer: 1
+    },
+    {
+        question: "Which of these is not a character in The Great Gatsby?",
+        choice1: "Daisy Buchanan",
+        choice2: "Jay Gatsby",
+        choice3:"Tom Sawyer",
+        choice4:  "Nick Carraway",
+        answer: 3
+    },
+    {
+        question: "What is the correct order of the Harry Potter books, starting with the first book?",
+        choice1: "Sorcerer's Stone, Chamber of Secrets, Prisoner of Azkaban, Goblet of Fire, Order of Phoenix, Half-Blood Prince, Deathly Hallows",
+        choice2: "Chamber of Secrets, Sorcerer's Stone, Prisoner of Azkaban, Goblet of Fire, Order of Phoenix, Half-Blood Prince, Deathly Hallows",
+        choice3: "Sorcerer's Stone, Chamber of Secrets, Goblet of Fire, Prisoner of Azkaban, Order of Phoenix, Half-Blood Prince, Deathly Hallows",
+        choice4:  "Sorcerer's Stone, Chamber of Secrets, Prisoner of Azkaban, Goblet of Fire, Half-Blood Prince, Order of Phoenix, Deathly Hallows",
+        answer: 1
+                
+        },
+        {
+            question: "In the novel Animal Farm by George Orwell, which animal represents Joseph Stalin?",
+            choice1: "Napoleon",
+            choice2: "Snowball",
+            choice3: "Squealer",
+            choice4:"Boxer",
+            answer: 1
+                        
+           
+        },
+        { 
+            question: "What is the name of the famous painting by Edvard Munch that depicts a person screaming?",
+            choice1: "The Scream",
+            choice2: "The Shout",
+            choice3: "The Cry",
+            choice4:  "The Yell",
+            answer: 1
+            },
+            {
+            question: "Which of these is not a type of sculpture?",
+            choice1: "Kinetic",
+            choice2: "Installation",
+            choice3:"Relief",
+            choice4:  "Hue",
+            answer: 4
+            
+            },
+            {
+            question: "What is the name of the French artist who created the painting Starry Night?",
+            choice1: "Vincent van Gogh",
+            choice2: "Claude Monet",
+            choice3: "Pablo Picasso",
+            choice4:  "Salvador Dali",
+            answer: 1
+                
+            },
+             {
+            question: "What is the name of the art style that uses bright colors and bold designs to create a psychedelic effect?",
+            choice1:"Pop Art",
+            choice2:"Cubism",
+            choice3:"Fauvism",
+            choice4:  "Op Art",
+            answer: 4
+                    
+            },
+            {
+            question: "Which of these is not a famous art museum in the world?",
+            choice1: "Louvre Museum",
+            choice2: "Tate Modern",
+            choice3: "Metropolitan Museum of Art",
+            choice4:  "Smithsonian National Air and Space Museum",
+            answer: 4
+                        
+           
+        },
+        { 
+            question: "What is the name of the British political party that is currently in power?",
+            choice1: "Labour Party",
+            choice2: "Liberal Democrats",
+            choice3: "Conservative Party",
+            choice4:  "Green Party",
+            answer: 3
+            },
+            {
+            question: "In the UK, what is the name of the event where voters cast their ballots in a general election?",
+            choice1: "Polling Day",
+            choice2: "Voting Day",
+            choice3: "Election Day",
+            choice4: "Democracy Day",
+            answer: 1
+            
+            },
+            {
+            question: "What is the name of the process of impeaching a US president?",
+            choice1: "Derailing",
+            choice2: "Overturning",
+            choice3: "Removal",
+            choice4:  "Impeachment",
+            answer: 4
+                
+            },
+             {
+            question: "Which of these countries was the first to give women the right to vote?",
+            choice1: "United States",
+            choice2: "Canada",
+            choice3: "New Zealand",
+            choice4:  "United Kingdom",
+            answer: 3
+                    
+            },
+            {
+            question: "What is the term for a person who is not a citizen of the country they are living in?",
+            choice1: "Foreigner",
+            choice2: "Alien",
+            choice3: "Stranger",
+            choice4:  "Visitor",
+            answer: 1
+                        
+            
+        },
+        {
+            question: "What is the highest mountain in the world?",
+            choice1: "Mount Kilimanjaro",
+            choice2: "Mount Everest",
+            choice3: "Mount Fuji",
+            choice4:  "Mount Olympus",
+            answer: 2
+            },
+            {
+            question: "Which of these countries is the smallest by land area?",
+            choice1: "Monaco",
+            choice2: "Singapore",
+            choice3:" Vatican City",
+            choice4:  "Liechtenstein",
+            answer: 3
+            
+            },
+            {
+            question: "What is the largest ocean in the world?",
+            choice1: "Indian Ocean",
+            choice2: "Atlantic Ocean",
+            choice3: "Pacific Ocean",
+            choice4:  "Arctic Ocean",
+            answer: 3
+                
+            },
+             {
+            question:"What is the name of the large desert in Africa that spans multiple countries?",
+            choice1:"Sahara Desert",
+            choice2: "Mojave Desert",
+            choice3: " Gobi Desert",
+            choice4:  "Atacama Desert",
+            answer: 1
+                    
+            },
+            {
+            question: "Which of these is not a European country?",
+            choice1: "Belgium",
+            choice2: "Jordan",
+            choice3: "Greece",
+            choice4:  "Norway",
+            answer: 2
+                        
+            },
+            { 
+                question: "What year did World War II end?",
+                choice1: "1945",
+                choice2: "1939",
+                choice3: "1951",
+                choice4: "1965",
+                answer: 1
+                },
+                {
+                question: "Which of these countries was not part of the Allied Powers in World War II?",
+                choice1: "United States",
+                choice2: "Soviet Union",
+                choice3: "Germany",
+                choice4:  "United Kingdom",
+                answer: 3
+                
+                },
+                {
+                question: "Who was the first president of the United States?",
+                choice1: " Thomas Jefferson",
+                choice2: "George Washington",
+                choice3: "John F. Kennedy",
+                choice4:  "Abraham Lincoln",
+                answer: 2
+                    
+                },
+                 {
+                question: "In what year did India gain independence from British rule?",
+                choice1: "1947",
+                choice2: "1951",
+                choice3: "1960",
+                choice4:  "1975",
+                answer: 1
+                        
+                },
+                {
+                question: "Which of these was not a Roman emperor?",
+                choice1: "Julius Caesar",
+                choice2: "Augustus",
+                choice3: "Nero",
+                choice4:  "Alexander the Great",
+                answer: 4
+                            
+                },
+                {
+                    question: "What is the most widely spoken language in the world?",
+                    choice1: "English",
+                    choice2: "Spanish",
+                    choice3: " Mandarin",
+                    choice4:  "French",
+                    answer: 3
+                    },
+                    {
+                    question: "Which language has the most words?",
+                    choice: "English",
+                    choice2: "Russian",
+                    choice3: "Arabic",
+                    choice4:  "German",
+                    answer: 1
+                    },
+                    {
+                        question: "What is the only language that is also an Olympic event?",
+                        choice1: "Gandalf",
+                        choice2: "French",
+                        choice3: "Spanish",
+                        choice4:  "English",
+                        answer: 4
+                        },
+                {
+                    question: "What is the longest word in the English language?",
+                    choice1: "Antidisestablishmentarianism",
+                    choice2: "Supercalifragilisticexpialidocious",
+                    choice3: "Pneumonoultramicroscopicsilicovolcanoconiosis",
+                    choice4:  "Hippopotomonstrosesquipedaliophobia",
+                    answer: 3
+                    },
+                    {
+                    question: "What language is spoken in Brazil?",
+                    choice1: "Portuguese",
+                    choice2: "Spanish",
+                    choice3: "French",
+                    choice4:  "Italian",
+                    answer: 1
+                    },
+                    {
+                    question: "What is the name of Beyoncé's alter ego?",
+                    choice1: "Sasha Fierce",
+                    choice2: "Lady Gaga",
+                    choice3: "Pink",
+                    choice4:  "Rihanna",
+                    answer: 1
+                    },
+                    {
+                    question: "Who wrote the hit song 'Thriller'?",
+                    choice1: "Michael Jackson",
+                    choice2: "Prince",
+                    choice3: "Madonna",
+                    choice4:  "Whitney Houston",
+                    answer: 1
+                    },
+                    {
+                    question: "Which of these is not a type of musical instrument?",
+                    choice1: "Trumpet",
+                    choice2: "Tuba",
+                    choice3: "Triangle",
+                    choice4:  "Trampoline",
+                    answer: 4
+                    },
+                    {
+                    question: "What is the name of the lead singer of the band Coldplay?",
+                    choice1: "Chris Martin",
+                    choice2: "Liam Gallagher",
+                    choice3: "Bono",
+                    choice4:  "Dave Grohl",
+                    answer: 1
+                    },
+                    {
+                    question: "Which band performed the hit song 'Stairway to Heaven'?",
+                    choice1: "The Rolling Stones",
+                    choice2: "Led Zeppelin",
+                    choice3: "Pink Floyd",
+                    choice4:  "The Beatles",
+                    answer: 2
+                    },
+                {   question: "What is the name of the largest muscle in the human body?",
+                    choice1: "Biceps",
+                    choice2: "Hamstring",
+                    choice3: "Gluteus maximus",
+                    choice4:  "Pectoralis major",
+                    answer: 3
+                    },
+                    {
+                    question: "What is the name of the system that controls the body's movements?",
+                    choice1: "Nervous system",
+                    choice2: "Digestive system",
+                    choice3: "Respiratory system",
+                    choice4:  "Cardiovascular system",
+                    answer: 1
+                    },
+                    {
+                    question: "What is the minimum amount of exercise recommended per day for adults?",
+                    choice1: "5 minutes",
+                    choice2: "30 minutes",
+                    choice3: "1 hour",
+                    choice4:  "2 hour",
+                    answer: 2
+                    },
+                    {
+                    question: "Which of these is not a type of stretching?",
+                    choice1: "Static stretching",
+                    choice2: "Dynamic stretching",
+                    choice3: "Acrobatic stretching",
+                    choice4:  "Ballistic stretching",
+                    answer: 3
+                    },
+                    {
+                    question: "What is the name of the highest score achievable in gymnastics?",
+                    choice1: "Perfect 10",
+                    choice2: "Superb 7",
+                    choice3: "Excellent 9",
+                    choice4:  "Magnificent 8",
+                    answer: 1
+                    }
+];
+
   const CORRECT_BONUS = 100;
   const MAX_QUESTIONS = 40;
   
